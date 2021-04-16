aacada3d76a152f7fa6b13a7f71585045c78879b
Muflih Kholidin <muflic.24@gmail.com>
2021-04-16 13:38:22 +0700

    Update Command Kernel

--- commands.php
+++ commands.php
@@ -2,6 +2,8 @@
 
 use Jakmall\Recruitment\Calculator\Commands\AddCommand;
 use Jakmall\Recruitment\Calculator\Commands\DivideCommand;
+use Jakmall\Recruitment\Calculator\Commands\HistoryClearCommand;
+use Jakmall\Recruitment\Calculator\Commands\HistoryListCommand;
 use Jakmall\Recruitment\Calculator\Commands\MultiplyCommand;
 use Jakmall\Recruitment\Calculator\Commands\PowCommand;
 use Jakmall\Recruitment\Calculator\Commands\SubCommand;
@@ -12,4 +14,6 @@ return [
     MultiplyCommand::class,
     DivideCommand::class,
     PowCommand::class,
+    HistoryClearCommand::class,
+    HistoryListCommand::class,
 ];
